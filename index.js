1158f8e8333cafba76903272cdfcc0e531e6ae44
Guido43 <gbsdpiano@yahoo.co.uk>
2023-07-03 12:13:05 +0200

    code added for req/res of endpoints

--- index.js
+++ index.js
@@ -20,8 +20,33 @@ app.use(morgan('common'));
 //responses to url requests
 app.get('/', (req, res) => {res.send('Welcome to my Movie club');
 });
-app.get('/movies', (req, res) => {res.json(topMovies);
-});
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+app.get('/movies/film_info', (req, res) => {
+    res.send('Successful GET request returning data on movie descriptions and other details of the movie');
+  });  
+app.get('/movies/genres', (req, res) => {
+    res.send('Succeessful GET request returning data on movie genres');
+  });
+app.get('/movies/directors', (req, res) => {
+    res.send('Successful GET request returning data on individual directors of the movies');
+  });
+app.post('/movies/users', (req, res) => {
+    res.send('Allows inputting of data to create user account');
+  });
+app.put('/movies/username', (req, res) => {
+    res.send('Allows users to update their account information');
+  });
+app.post('/movies/add_movie', (req, res) => {
+    res.send('Allows users to add a movie of their choice to the list');
+  });
+app.delete('/movies/remove_movie', (req, res) => {
+    res.send('Allows users to remove a movie from their list');
+  });
+app.delete('/movies/remove_user', (req, res) => {
+    res.send('Allows existing users to unregister and remove their account');
+  });
 
 //documentation file from 'public' folder
 app.use(express.static('public'));
