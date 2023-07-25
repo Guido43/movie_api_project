0f615d6103772ae3b0870130f998f671e10b885f
Guido43 <gbsdpiano@yahoo.co.uk>
2023-07-25 18:12:01 +0100

    get all users commented out for security

--- index.js
+++ index.js
@@ -42,16 +42,16 @@ app.get('/', (req, res) => {res.send('Welcome to my Movie club');
 
 
 // Get all users(Read)
-app.get('/users', (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.status(201).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
+//app.get('/users', (req, res) => {
+  //Users.find()
+    //.then((users) => {
+      //res.status(201).json(users);
+    //})
+    //.catch((err) => {
+      //console.error(err);
+      //res.status(500).send('Error: ' + err);
+    //});
+//});
 
 //Get all movies(Read)
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req,res) => {
