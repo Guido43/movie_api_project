c137fe58acdadc074c1f0e7e3e8c0c44c24962e4
Guido43 <gbsdpiano@yahoo.co.uk>
2023-07-25 15:04:12 +0100

    updated for hashing passwords

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
@@ -16,13 +17,22 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean,
 });
 
+
 let userSchema = mongoose.Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
     birthday: Date,
-    favoritemovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
-});
+    favoritemovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  });
+  
+  userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+  };
 
 
 let Movie = mongoose.model('Movie', movieSchema);
