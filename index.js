d8411454bf3df0a94e33a86311295105da7a983e
Guido43 <gbsdpiano@yahoo.co.uk>
2023-09-26 11:54:00 +0200

    get users unlocked for testing purposes

--- index.js
+++ index.js
@@ -60,16 +60,16 @@ app.get('/movies', passport.authenticate('jwt', { session: false}),
 });
 
 //Get all users(Read) this retturns a list of all users
-//app.get('/users', (req, res) => {
-  //Users.find()
-  //.then((movies) => {
-    //res.status(201).json(movies);
-//})
-  //.catch((err) => {
-    //console.error(err);
-    //res.status(500).send('Error:  ' + err);
-  //});
-//});
+app.get('/users', (req, res) => {
+  Users.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+})
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error:  ' + err);
+  });
+});
 
 
 
